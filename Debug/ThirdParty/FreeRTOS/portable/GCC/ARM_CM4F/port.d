ecfb266487f807b8d55403791d6347eaeb00c38e
NHHanh <huuhanh010602@gmail.com>
2023-03-23 16:57:13 +0700

    queue

--- Debug/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d
+++ Debug/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d
@@ -1,29 +1,20 @@
 ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.o: \
  ../ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.c \
- C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/FreeRTOS.h \
- C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
- C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/projdefs.h \
- C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/portable.h \
- C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
- C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
- C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
- C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/task.h \
- C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/list.h
-
-C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/FreeRTOS.h:
-
-C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
-
-C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/projdefs.h:
-
-C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/portable.h:
-
-C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
-
-C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
-
-C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
-
-C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/task.h:
-
-C:/Users/DELL/Desktop/Study_FreeRTOS/Workspace/RTOS_workspace/F4_QueueUart/ThirdParty/FreeRTOS/include/list.h:
+ C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/list.h
+C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/NHHanh/Desktop/RTOS_Queue/ThirdParty/FreeRTOS/include/list.h:
